138d14f9c29246945462754073a1c335cfa38c4b
Chaithanya Dileep THONDAMANATI <c.thondamanati@f5.com>
2024-07-30 16:10:08 +0530

    new code 3

--- xc/xc_loadbalancer.tf
+++ xc/xc_loadbalancer.tf
@@ -4,14 +4,7 @@ resource "volterra_service_policy" "allow_all" {
     algo        = "FIRST_MATCH"
     any_server  = true
 
-    simple_rules {
-        action = "ALLOW"
-        waf_action {
-            none = true
-            waf_in_monitoring_mode = false
-            waf_skip_processing    = false
-        }
-    }
+
 
     allow_all_requests = true
     deny_all_requests  = false
