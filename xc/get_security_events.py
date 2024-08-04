c66d63f752f073a54532988eb5cae205743a45cb
Chaithanya Dileep THONDAMANATI <c.thondamanati@f5.com>
2024-08-04 15:24:05 +0530

    curl added:

--- xc/get_security_events.py
+++ xc/get_security_events.py
@@ -2,6 +2,7 @@ import json
 
 def initial_testing():
     print("hello there")
+    curl -v -k --cert-type P12 --cert treino.console.ves.volterra.io.api-creds_ideathon.p12:Nasagov7$ -X POST https://treino.console.ves.volterra.io/api/data/namespaces/kvm-on-prem/app_security/events
 
 if __name__ == "__main__":
     initial_testing()
