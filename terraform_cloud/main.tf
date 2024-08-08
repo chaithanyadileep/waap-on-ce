f3fbb865a1374bb4baaf8161a8327a2fedcffee9
Chaithanya Dileep THONDAMANATI <c.thondamanati@f5.com>
2024-08-08 18:11:20 +0530

    run cloud

---
+++ terraform_cloud/main.tf
@@ -0,0 +1,3 @@
+resource "some_resource" "a" {
+  ip_address    = var.image_id
+}
---
+++ terraform_cloud/variables.tf
@@ -0,0 +1,5 @@
+variable "image_id" {
+  description = "Workspace name of Azure deployment infra"
+  type        = string
+  default     = "ami-abc123"
+}
