658b57764f930060396ecc17211fb4eb9fc32b36
Chaithanya Dileep THONDAMANATI <c.thondamanati@f5.com>
2024-08-08 15:31:31 +0530

    new line

--- python_data/ip_process.py
+++ python_data/ip_process.py
@@ -1,7 +1,10 @@
 import sys
-def deploy_app(pub_ip):
-    print(pub_ip)
+def print_val():
+    return ["1.1.1.1", "2.2.2.2"]
+
+
 
 if __name__ == "__main__":
-    pub_ip = sys.argv[1]
-    deploy_app(pub_ip)
+    #pub_ip = sys.argv[1]
+    #deploy_app(pub_ip)
+    print-val()
